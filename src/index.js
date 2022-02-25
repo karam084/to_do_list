8180f8bee7996905db431ea7bed1b6c4b144dc73
karam084 <karam.ezz84@gmail.com>
2022-02-25 15:39:36 +0200

    Fix linter errors4

--- src/index.js
+++ src/index.js
@@ -35,7 +35,30 @@ const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   const checkBox = taskListItem.querySelector('input[type=checkbox]');
   const editButton = taskListItem.querySelector('button.edit');
   const deleteButton = taskListItem.querySelector('button.delete');
+  let editTask = function () {
+    console.log('Edit Task...');
+    console.log("Change 'edit' to 'save'");
 
+    let listItem = this.parentNode;
+
+    let editInput = listItem.querySelector('input[type=text]');
+    let label = listItem.querySelector('label');
+    let containsClass = listItem.classList.contains('editMode');
+    if (containsClass) {
+      label.innerText = editInput.value;
+    } else {
+      editInput.value = label.innerText;
+    }
+
+    listItem.classList.toggle('editMode');
+  };
+  const deleteTask = function () {
+    console.log('Delete Task...');
+
+    let listItem = this.parentNode;
+    let ul = listItem.parentNode;
+    ul.removeChild(listItem);
+  };
   editButton.onclick = editTask;
   deleteButton.onclick = deleteTask;
   checkBox.onchange = checkBoxEventHandler;
@@ -50,47 +73,20 @@ const addTask = function () {
     bindTaskEvents(listItem, taskIncomplete);
   };
 
+  const taskIncomplete = function () {
+    console.log('Incomplete Task...');
+
+    const listItem = this.parentNode;
+    incompleteTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+  };
   incompleteTaskHolder.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 
   taskInput.value = '';
 };
 
-let editTask = function () {
-  console.log('Edit Task...');
-  console.log("Change 'edit' to 'save'");
-
-  let listItem = this.parentNode;
-
-  let editInput = listItem.querySelector('input[type=text]');
-  let label = listItem.querySelector('label');
-  let containsClass = listItem.classList.contains('editMode');
-  if (containsClass) {
-    label.innerText = editInput.value;
-  } else {
-    editInput.value = label.innerText;
-  }
-
-  listItem.classList.toggle('editMode');
-};
-
-let deleteTask = function () {
-  console.log('Delete Task...');
-
-  let listItem = this.parentNode;
-  let ul = listItem.parentNode;
-  ul.removeChild(listItem);
-};
-
-let taskIncomplete = function () {
-  console.log('Incomplete Task...');
-
-  let listItem = this.parentNode;
-  incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
-};
-
-let ajaxRequest = function () {
+const ajaxRequest = function () {
   console.log('AJAX Request');
 };
 
@@ -98,7 +94,7 @@ addButton.onclick = addTask;
 addButton.addEventListener('click', addTask);
 addButton.addEventListener('click', ajaxRequest);
 
-for (let i = 0; i < incompleteTaskHolder.children.length; i++) {
+for (const i = 0; i < incompleteTaskHolder.children.length; i++) {
   bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
 }
 for (i = 0; i < completedTasksHolder.children.length; i++) {
