6a6bec3609b4cda770ce17987ae4e63e95a7c5ee
karam084 <karam.ezz84@gmail.com>
2022-02-25 14:51:31 +0200

    Fix linter errors3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+
 const taskInput = document.getElementById('new-task');
 const addButton = document.getElementsByTagName('button')[0];
 const incompleteTaskHolder = document.getElementById('incomplete-tasks');
@@ -29,67 +30,6 @@ const createNewTaskElement = function (taskString) {
   listItem.appendChild(deleteButton);
   return listItem;
 };
-
-const addTask = function () {
-  console.log('Add Task...');
-  const listItem = createNewTaskElement(taskInput.value);
-
-  incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
-
-  taskInput.value = '';
-};
-
-const editTask = function () {
-  console.log('Edit Task...');
-  console.log("Change 'edit' to 'save'");
-
-  const listItem = this.parentNode;
-
-  const editInput = listItem.querySelector('input[type=text]');
-  const label = listItem.querySelector('label');
-  const containsClass = listItem.classList.contains('editMode');
-  if (containsClass) {
-    label.innerText = editInput.value;
-  } else {
-    editInput.value = label.innerText;
-  }
-
-  listItem.classList.toggle('editMode');
-};
-
-const deleteTask = function () {
-  console.log('Delete Task...');
-
-  const listItem = this.parentNode;
-  const ul = listItem.parentNode;
-  ul.removeChild(listItem);
-};
-
-const taskCompleted = function () {
-  console.log('Complete Task...');
-
-  const listItem = this.parentNode;
-  completedTasksHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskIncomplete);
-};
-
-const taskIncomplete = function () {
-  console.log('Incomplete Task...');
-
-  const listItem = this.parentNode;
-  incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
-};
-
-const ajaxRequest = function () {
-  console.log('AJAX Request');
-};
-
-addButton.onclick = addTask;
-addButton.addEventListener('click', addTask);
-addButton.addEventListener('click', ajaxRequest);
-
 const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log('bind list item events');
   const checkBox = taskListItem.querySelector('input[type=checkbox]');
@@ -100,14 +40,67 @@ const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   deleteButton.onclick = deleteTask;
   checkBox.onchange = checkBoxEventHandler;
 };
+const addTask = function () {
+  console.log('Add Task...');
+  const listItem = createNewTaskElement(taskInput.value);
+  const taskCompleted = function () {
+    console.log('Complete Task...');
+    let listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+  };
 
-for (const i = 0; i < incompleteTaskHolder.children.length; i++) {
+  incompleteTaskHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+
+  taskInput.value = '';
+};
+
+let editTask = function () {
+  console.log('Edit Task...');
+  console.log("Change 'edit' to 'save'");
+
+  let listItem = this.parentNode;
+
+  let editInput = listItem.querySelector('input[type=text]');
+  let label = listItem.querySelector('label');
+  let containsClass = listItem.classList.contains('editMode');
+  if (containsClass) {
+    label.innerText = editInput.value;
+  } else {
+    editInput.value = label.innerText;
+  }
+
+  listItem.classList.toggle('editMode');
+};
+
+let deleteTask = function () {
+  console.log('Delete Task...');
+
+  let listItem = this.parentNode;
+  let ul = listItem.parentNode;
+  ul.removeChild(listItem);
+};
+
+let taskIncomplete = function () {
+  console.log('Incomplete Task...');
+
+  let listItem = this.parentNode;
+  incompleteTaskHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+};
+
+let ajaxRequest = function () {
+  console.log('AJAX Request');
+};
+
+addButton.onclick = addTask;
+addButton.addEventListener('click', addTask);
+addButton.addEventListener('click', ajaxRequest);
+
+for (let i = 0; i < incompleteTaskHolder.children.length; i++) {
   bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
 }
-for (const i = 0; i < completedTasksHolder.children.length; i++) {
+for (i = 0; i < completedTasksHolder.children.length; i++) {
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
-
-window.addEventListener('DOMContentLoaded', () => {
-  slist(document.getElementsByClassName('sortlist'));
-});
